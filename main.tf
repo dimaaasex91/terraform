795cc7b18c4a41e43310ae6f309097a881152e66
dimaaasex911 <dimaaasex911@gmail.com>
2021-11-25 12:28:37 +0200

    add secure group and open 8080 port

--- main.tf
+++ main.tf
@@ -9,16 +9,31 @@
 # }
 
 provider "aws" {
-  region         = "us-east-2"
+  region     = "us-east-2"
   access_key = "AKIAQFO7CBOZGYO5RHNA"
   secret_key = "/n6JHztgI4/suXvJbvEhBXIfbFZJwTXCF3hI3e5N"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data              = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
   tags = {
     "Name" = "my_instance"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "my_instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
